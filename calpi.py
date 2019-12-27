e84c32a77c7a5ff150237d5e3ce28bae4133cd65
Gxstar <gengxing123@qq.com>
2019-12-27 15:46:48 +0800

    Update calpi.py
    添加一种求Π的方法

--- calpi.py
+++ calpi.py
@@ -23,13 +23,29 @@ def startProcess(total,tNum):#创建进程开始执行
     for i in results:
         pots=pots+i.get()
     return pots
-def main():
+def monte():
     x=int(input("请输入抛入点数量（数量越大结果越准）："))
     y=int(input("请输入调用进程数（最大为8）：")) 
     start=time.time()
     results=startProcess(x,y)
-    print("Π=",4*results/x)
     print("程序运行时间为：{}秒".format(time.time()-start))
+    return 4*results/x
+def limit():
+    n=int(input("请输入常数n（越大越精确）："))
+    sum=0
+    start=time.time()
+    for i in range(n):
+        sum=sum+1.0/((i+1)*(i+1))
+    pi=sqrt(sum*6)
+    print("程序运行时间为：{}秒".format(time.time()-start))
+    return pi
+def main():
+    method=input("请输入1，2选择计算方法\n1.蒙特卡洛法：通过抛撒大量随机点计算\n2.利用极限公式计算\n")
+    if method=='1':
+        pi=monte()
+    else:
+        pi=limit()
+    print("Π=",pi)
 if __name__=="__main__":
     multiprocessing.freeze_support()
     main()
